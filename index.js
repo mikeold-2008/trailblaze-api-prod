e2b2f90444f310a2702d2567daebadfad6ee8cc9
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2024-05-23 23:04:50 +0100

    removes app listener, adds activities endpoints and its routes

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-require('dotenv').config()
-const express = require('express');
-const mongoose = require('mongoose');
-const userRoutes = require('./routes/userRoutes');
-const cors = require('cors'); 
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
+const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 9090;
-const uri = process.env.MONGO_URI
+const uri = process.env.MONGO_URI;
+const activityRouter = require("./routes/activityRoutes");
 
 // Connect to MongoDB
-mongoose.connect(uri);
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  dbName: "trailblaze",
+});
 
+mongoose.connection.on("connected", () => {
+  console.log("Connected to the Database");
+});
 // Use the cors middleware to enable Cross-Origin Resource Sharing
 app.use(cors());
 app.use(express.json());
-app.use('/users', userRoutes);
-app.listen(port, () => {
- console.log(`Server is running on port ${port}`);
-});
+app.use("/users", userRoutes);
+app.use("/activities", activityRouter);
+
+module.exports = app;
