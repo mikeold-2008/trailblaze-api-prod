ccd20ed7ba9229242f0b15966066e17fd4b31918
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2024-05-26 23:21:14 +0100

    adds conditional statement for connecting to dev database

--- index.js
+++ index.js
@@ -4,23 +4,30 @@ const mongoose = require("mongoose");
 const userRoutes = require("./routes/userRoutes");
 const cors = require("cors");
 const app = express();
-const uri = process.env.MONGO_URI;
+const { MONGO_URI, DB_NAME, NODE_ENV } = process.env;
 const activityRouter = require("./routes/activityRoutes");
 
 // Connect to MongoDB
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  dbName: "trailblaze",
-});
+if (!NODE_ENV === test) {
+  mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: DB_NAME,
+  });
 
-mongoose.connection.on("connected", () => {
-  console.log("Connected to the Database");
-});
+  mongoose.connection.on("connected", () => {
+    console.log("Connected to the Database");
+  });
+}
+
 // Use the cors middleware to enable Cross-Origin Resource Sharing
 app.use(cors());
 app.use(express.json());
 app.use("/users", userRoutes);
 app.use("/activities", activityRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: err._message });
+});
 
 module.exports = app;
