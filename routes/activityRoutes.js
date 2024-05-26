1f71d2d40bac6d6f35022998dfc7ec08a3b2fe47
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2024-05-26 23:21:47 +0100

    adds get activities by user end point

--- routes/activityRoutes.js
+++ routes/activityRoutes.js
@@ -4,12 +4,16 @@ const {
   getAllActivities,
   getSingleActivity,
   deleteOneActivity,
+  postOneActivity,
+  updateOneActivity,
+  getActivitiesByUser,
 } = require("../controllers/activityController");
 
 activityRouter.get("/", getAllActivities);
 activityRouter.get("/:activity_id", getSingleActivity);
 activityRouter.delete("/:activity_id", deleteOneActivity);
-// activityRouter.post("/", postOneActivity);
-// activityRouter.patch("/", update...);
+activityRouter.post("/", postOneActivity);
+activityRouter.patch("/:activity_id", updateOneActivity);
+activityRouter.get("/user/:user_id", getActivitiesByUser);
 
 module.exports = activityRouter;
