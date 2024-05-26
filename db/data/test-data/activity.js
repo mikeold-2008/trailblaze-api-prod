8699ccaa10abe0a3ed7b21e9057a0da16e6aafe8
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2024-05-26 23:22:05 +0100

    adds db folder to store test data

---
+++ db/data/test-data/activity.js
@@ -0,0 +1,10 @@
+module.exports = [
+  { activity_id: 1, exercise_name: "running", user_id: 1 },
+  { activity_id: 2, exercise_name: "walking", user_id: 2 },
+  { activity_id: 3, exercise_name: "cycling", user_id: 1 },
+  { activity_id: 4, exercise_name: "cycling", user_id: 3 },
+  { activity_id: 5, exercise_name: "cycling", user_id: 5 },
+  { activity_id: 6, exercise_name: "cycling", user_id: 6 },
+  { activity_id: 7, exercise_name: "walking", user_id: 2 },
+  { activity_id: 8, exercise_name: "walking", user_id: 2 },
+];
